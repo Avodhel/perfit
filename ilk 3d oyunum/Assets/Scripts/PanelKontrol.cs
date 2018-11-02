218e9f2e6565992a65098e8e81a5b46d74aba5f8
nordicebear <nordicebeargames@gmail.com>
2018-11-02 23:55:15 +0300

    ilk 3D oyunum v0.1.2

---
+++ ilk 3d oyunum/Assets/Scripts/PanelKontrol.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelKontrol : MonoBehaviour {
+
+    float speed = 40;
+
+    void Start ()
+    {
+
+    }
+	
+	void Update ()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0); //sağa veya sola döndür
+    }
+
+    void OnCollisionEnter(Collision col) //palete çarpan şekillerin hangi açıdan çarptığını saptama
+    {
+        if (col.gameObject.tag.Equals("squareTag"))
+        {
+            Vector3 hit = col.contacts[0].normal;
+            Debug.Log(hit);
+            float angle = Vector3.Angle(hit, Vector3.up);
+
+            if (Mathf.Approximately(angle, 0))
+            {
+                //Down
+                Debug.Log("Down");
+            }
+            if (Mathf.Approximately(angle, 180))
+            {
+                //Up
+                Debug.Log("Up");
+            }
+            if (Mathf.Approximately(angle, 90))
+            {
+                // Sides
+                Vector3 cross = Vector3.Cross(Vector3.forward, hit);
+                if (cross.y > 0)
+                { // left side of the player
+                    Debug.Log("Left");
+                }
+                else
+                { // right side of the player
+                    Debug.Log("Right");
+                }
+            }
+        }
+    }
+}
---
+++ ilk 3d oyunum/Assets/Scripts/SekilKontrol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SekilKontrol : MonoBehaviour {
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        sekilDondur();
+	}
+
+    void sekilDondur()
+    {
+        transform.Rotate(new Vector3(0, Random.Range(0f, 360f), 0) * (Time.deltaTime * 0.1f));
+    }
+}
