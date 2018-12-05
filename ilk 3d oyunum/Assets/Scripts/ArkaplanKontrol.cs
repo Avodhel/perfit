7893e31a17173c003665d55b8d593af56b636c9d
nordicebear <nordicebeargames@gmail.com>
2018-12-06 02:03:52 +0300

    ilk 3D oyunum v0.2.3 (ArkaplanKontrol scripti eklendi)

---
+++ ilk 3d oyunum/Assets/Scripts/ArkaplanKontrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArkaplanKontrol : MonoBehaviour {
+
+    private float cycleSeconds = 500f; // set to say 0.5f to test
+    float renkSinir1;
+    float renkSinir2;
+
+    private void Start()
+    {
+        renkSinirBelirle();
+    }
+
+    void Update()
+    {
+        arkaplanRenkDegistir();
+    }
+
+    public void renkSinirBelirle()
+    {
+        renkSinir1 = Random.Range(0.4f, 0.8f);
+        renkSinir2 = Random.Range(0.4f, 0.8f);
+    }
+
+    void arkaplanRenkDegistir()
+    {
+        GetComponent<Renderer>().material.color = Color.HSVToRGB
+            (
+             Mathf.Repeat(Time.time / cycleSeconds, 1f),
+             renkSinir1,     // set to a pleasing value. 0f to 1f
+             renkSinir2      // set to a pleasing value. 0f to 1f
+            );
+    }
+}
