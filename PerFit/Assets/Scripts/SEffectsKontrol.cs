872e0f27aafdbd800e5d1116eb2186d54d1fb2bd
nordicebear <nordicebeargames@gmail.com>
2019-03-08 00:31:16 +0300

    güncelleme öncesi ayarlamalar

--- PerFit/Assets/Scripts/SEffectsKontrol.cs
+++ PerFit/Assets/Scripts/SEffectsKontrol.cs
@@ -145,7 +145,7 @@ public class SEffectsKontrol : MonoBehaviour {
             effectAlert.enabled = true;
             effectAlert.overrideSprite = expand;
 
-            Panel.transform.localScale = new Vector3(1.85f, Panel.transform.localScale.y, 1.85f);
+            Panel.transform.localScale = new Vector3(1.8f, Panel.transform.localScale.y, 1.8f);
             yield return new WaitForSeconds(expandEffectTime);
             Panel.transform.localScale = new Vector3(1.7f, Panel.transform.localScale.y, 1.7f);
 
